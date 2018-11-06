dd96c6aba1e25a81e8b66fd08fee55c6d4532476
Don Hill <ptsdon@gmail.com>
2018-11-06 16:23:50 -0500

    your commit message

---
+++ if-test/if-hostname.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+hostname = 'MTG'
+input.lower("What hostname are you looking for? ")
+if hostname = True:
+    print('The hostname was found to be mtg')
+else:
+    print("Hostname not found")
+exit()
+
---
+++ if-test2/iftest2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ') # this line now prompts the user for input
+
+if ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+elif ipchk: # if any data is provided, this will test true
+   print('Looks like the IP address was set: ' + ipchk) # indented under if
+else: # if data is NOT provided
+   print('You did not provide input.') # indented under else
+
+
