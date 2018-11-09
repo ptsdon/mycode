5b3c9610b4f64a617681c0c30b1ddb6057775ae1
Don Hill <ptsdon@gmail.com>
2018-11-09 12:48:14 -0500

    lab 25 and 26

---
+++ montypython/monty_python2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+round = 0        # integer round initiated to 0
+
+while(True):     # sets up an infinite loop condition
+
+    round = round + 1    # increase the round counter
+    print('''Finish the movie title, "Monty Python/'s The Life of ______"''')
+    answer = input() # string answer collected from user
+    if (answer == 'Brian'): # logic check, user gave correct answer
+        print('Correct!')
+        break            # break statement escapes the while loop
+    elif(round==3):  # logic to ensure round has not yet reached 3
+        print('Sorry, the answer was Brian.')
+        break            # break statement escapes the while loop
+    else:
+        print('Sorry! Try again!')
+
